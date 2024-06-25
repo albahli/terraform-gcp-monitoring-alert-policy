cff32c8de4f040b18a7db6fee663c82ae49b3fba
f.albahli <f.albahli@takamol.com.sa>
2024-06-25 11:44:35 +0300

    Adding TF custom module

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "5.35.0"
-    }
-  }
-}
-
 provider "google" {
   project = var.project
   region  = var.region
